0317b2371d1def09b55a240830bf9aba3843d2a4
speane <avtolubitel225@gmail.com>
2016-09-20 17:34:02 +0300

    Refactored request parser

--- src/main/java/com/evgenyshilov/web/contacts/help/RequestParser.java
+++ src/main/java/com/evgenyshilov/web/contacts/help/RequestParser.java
@@ -2,7 +2,6 @@ package com.evgenyshilov.web.contacts.help;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 /**
  * Created by Evgeny Shilov on 20.09.2016.
@@ -18,8 +17,6 @@ public class RequestParser {
     public ArrayList<Integer> getCheckedIdList(String checkboxGroupName) {
         String[] checkedValues = request.getParameterValues(checkboxGroupName);
         ArrayList<Integer> checkedIdList = new ArrayList<>();
-
-        System.out.println(Arrays.toString(checkedValues));
 
         if (checkedValues != null) {
             for (String value : checkedValues) {
