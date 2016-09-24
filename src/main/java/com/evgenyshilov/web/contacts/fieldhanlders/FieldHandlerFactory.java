e51c63d3cc5516d803f44bccc43342fc7c63b6f3
speane <avtolubitel225@gmail.com>
2016-09-24 13:42:13 +0300

    Added field handler factory

---
+++ src/main/java/com/evgenyshilov/web/contacts/fieldhanlders/FieldHandlerFactory.java
@@ -0,0 +1,34 @@
+package com.evgenyshilov.web.contacts.fieldhanlders;
+
+import java.util.HashMap;
+
+/**
+ * Created by Evgeny Shilov on 24.09.2016.
+ */
+public class FieldHandlerFactory {
+    private HashMap<String, FieldHandler> fieldHandlerMap;
+
+    public FieldHandlerFactory() {
+        fieldHandlerMap = new HashMap<>();
+        fieldHandlerMap.put("first-name", new FirstNameFieldHandler());
+        fieldHandlerMap.put("last-name", new LastNameFieldHandler());
+        fieldHandlerMap.put("patronymic", new PatronymicFieldHanlder());
+        fieldHandlerMap.put("day", new DayFieldHandler());
+        fieldHandlerMap.put("month", new MonthFieldHandler());
+        fieldHandlerMap.put("year", new YearFieldHanlder());
+        fieldHandlerMap.put("sex", new SexFieldHanlder());
+        fieldHandlerMap.put("marital-status", new MaritalStatusFieldHandler());
+        fieldHandlerMap.put("website", new WebsiteFieldHandler());
+        fieldHandlerMap.put("email", new EmailFieldHandler());
+        fieldHandlerMap.put("job", new JobFieldHandler());
+        fieldHandlerMap.put("state", new StateFieldHandler());
+        fieldHandlerMap.put("city", new CityFieldHandler());
+        fieldHandlerMap.put("house", new HouseFieldHandler());
+        fieldHandlerMap.put("flat", new FlatFieldHandler());
+        fieldHandlerMap.put("zipcode", new ZipCodeFieldHandler());
+    }
+
+    public FieldHandler getFieldHandler(String fieldName) {
+        return fieldHandlerMap.get(fieldName);
+    }
+}
