7335d346380c363e7ed8235f4c16b13ffad2c744
speane <avtolubitel225@gmail.com>
2016-09-29 03:08:46 +0300

    Refactored edit checked contacts command

--- src/main/java/com/evgenyshilov/web/contacts/commands/EditCheckedContactCommand.java
+++ src/main/java/com/evgenyshilov/web/contacts/commands/EditCheckedContactCommand.java
@@ -1,9 +1,11 @@
 package com.evgenyshilov.web.contacts.commands;
 
+import com.evgenyshilov.web.contacts.exceptions.CustomException;
 import com.evgenyshilov.web.contacts.help.RequestParser;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.ArrayList;
 
 /**
@@ -12,14 +14,20 @@ import java.util.ArrayList;
 public class EditCheckedContactCommand implements Command {
 
     @Override
-    public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+    public String execute(HttpServletRequest request, HttpServletResponse response) throws CustomException {
+        String redirectURL;
         ArrayList<Integer> checkedIds = new RequestParser(request).getCheckedIdList("contact-check");
         if (!checkedIds.isEmpty()) {
             int checkedContactId = checkedIds.get(0);
             request.setAttribute("id", checkedContactId);
-            response.sendRedirect("/app/edit-contact?id=" + checkedContactId);
+            redirectURL = "/app/edit-contact?id=" + checkedContactId;
         } else {
-            response.sendRedirect("/app/contact-list");
+            redirectURL = "/app/contact-list";
+        }
+        try {
+            response.sendRedirect(redirectURL);
+        } catch (IOException e) {
+            throw new CustomException("Can't redirect to url: ", e);
         }
         return null;
     }
