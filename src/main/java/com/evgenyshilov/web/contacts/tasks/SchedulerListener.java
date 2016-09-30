de389513d42c52087111ae48c40544d62042b6cf
speane <avtolubitel225@gmail.com>
2016-09-30 21:16:24 +0300

    Refactored scheduler listener

--- src/main/java/com/evgenyshilov/web/contacts/tasks/SchedulerListener.java
+++ src/main/java/com/evgenyshilov/web/contacts/tasks/SchedulerListener.java
@@ -1,40 +1,60 @@
 package com.evgenyshilov.web.contacts.tasks;
 
+import com.evgenyshilov.web.contacts.resources.ApplicationConfig;
+import org.quartz.*;
+import org.quartz.impl.StdSchedulerFactory;
+
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+
+import static org.quartz.CronScheduleBuilder.dailyAtHourAndMinute;
 
 
 /**
  * Created by Evgeny Shilov on 25.09.2016.
  */
 public class SchedulerListener implements ServletContextListener {
-    //private Scheduler scheduler;
+    private Scheduler scheduler;
 
     @Override
     public void contextInitialized(ServletContextEvent servletContextEvent) {
-        /*SchedulerFactory factory = new StdSchedulerFactory();
+        SchedulerFactory factory = new StdSchedulerFactory();
+        String JOB_NAME = "emailJob";
+        String JOB_GROUP = "emailJobGroup";
         try {
             scheduler = factory.getScheduler();
             JobDetail job = JobBuilder.newJob(SendEmailJob.class)
-                    .withIdentity("myJob", "group1") // name "myJob", group "group1"
+                    .withIdentity(JOB_NAME, JOB_GROUP) // name "myJob", group "group1"
                     .build();
+
             Trigger trigger = TriggerBuilder.newTrigger()
-                    .withIdentity("myJob", "group1")
-                    .withSchedule(dailyAtHourAndMinute(1, 2))
+                    .withIdentity(JOB_NAME, JOB_GROUP)
+                    .withSchedule(dailyAtHourAndMinute(getIntProperty("EMAIL_SEND_HOUR"),
+                            getIntProperty("EMAIL_SEND_MINUTE")))
                     .build();
             scheduler.start();
             scheduler.scheduleJob(job, trigger);
         } catch (SchedulerException e) {
-            e.printStackTrace();
-        }*/
+            // TODO log exception
+        }
+    }
+
+    private int getIntProperty(String name) {
+        try {
+            String value = ApplicationConfig.getProperty(name);
+            return value != null ? Integer.parseInt(value) : 0;
+        } catch (NumberFormatException e) {
+            // TODO log exception
+        }
+        return 0;
     }
 
     @Override
     public void contextDestroyed(ServletContextEvent servletContextEvent) {
-        /*try {
+        try {
             scheduler.shutdown(false);
         } catch (SchedulerException e) {
-            e.printStackTrace();
-        }*/
+            // TODO log exception
+        }
     }
 }
--- src/main/java/com/evgenyshilov/web/contacts/tasks/SendEmailJob.java
+++ src/main/java/com/evgenyshilov/web/contacts/tasks/SendEmailJob.java
@@ -1,30 +1,34 @@
 package com.evgenyshilov.web.contacts.tasks;
 
+import com.evgenyshilov.web.contacts.database.dao.ContactDAO;
+import com.evgenyshilov.web.contacts.database.dao.DAOFactory;
+import com.evgenyshilov.web.contacts.database.model.Contact;
+import com.evgenyshilov.web.contacts.email.EmailSender;
+import org.joda.time.DateTime;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
-import org.quartz.JobExecutionException;
+
+import java.util.ArrayList;
 
 /**
  * Created by Evgeny Shilov on 25.09.2016.
  */
 public class SendEmailJob implements Job {
     @Override
-    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
-        /*EmailSender emailSender = new EmailSender();
+    public void execute(JobExecutionContext jobExecutionContext) {
+        EmailSender emailSender = new EmailSender();
         try {
             ContactDAO contactDAO = (ContactDAO) DAOFactory.getDAO(Contact.class);
             ArrayList<Contact> todayBirthDayContacts = new ArrayList<>();
             for (Contact contact : contactDAO.getAll()) {
                 DateTime birthDay = new DateTime(contact.getBirthday().getTime());
                 DateTime today = DateTime.now();
-                System.out.println(birthDay + " " + today);
                 if (birthDay.getMonthOfYear() == today.getMonthOfYear() &&
                         birthDay.getDayOfMonth() == today.getDayOfMonth()) {
                     todayBirthDayContacts.add(contact);
                 }
             }
             String mailText;
-            System.out.println("228");
             if (todayBirthDayContacts.size() == 0) {
                 mailText = "Сегодня никто не празднует день рождения =(";
             } else {
@@ -38,6 +42,6 @@ public class SendEmailJob implements Job {
             emailSender.sendEmail("avtolubitel225@gmail.com", mailText);
         } catch (IllegalAccessException | InstantiationException | SQLException | NoSuchMethodException | InvocationTargetException | MessagingException e) {
             e.printStackTrace();
-        }*/
+        }
     }
 }
